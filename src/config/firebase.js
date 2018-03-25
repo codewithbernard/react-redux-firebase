73aaa9c9c8628080b5362f8472ec5bb4715cb4f1
Bernard Bado <bado.bernard94@gmail.com>
2018-03-25 14:04:22 +0200

    Fixing firebase config import

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import * as firebase from "firebase";
 
-firebase.initializeApp(require("./keys"));
+import { FirebaseConfig } from "../config/keys";
+firebase.initializeApp(FirebaseConfig);
 
 const databaseRef = firebase.database().ref();
 export const todosRef = databaseRef.child("todos");
